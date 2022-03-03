d2b6d535b5c67a3ffa10f795a391c1901518055d
pazzi <carlos.pazzianotto@gmail.com>
2022-03-03 17:24:33 -0300

    Conexao com Mongodb com repository e service

---
+++ src/main/java/com/carlospazzianotto/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.carlospazzianotto.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carlospazzianotto.workshopmongo.domain.User;
+import com.carlospazzianotto.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
